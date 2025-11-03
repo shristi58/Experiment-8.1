b162887c3cd8eb2dfa2efffa3bcd4d2707716bd7
shristi58 <kumarishristi22004@gmail.com>
2025-11-03 19:40:33 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Login";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
